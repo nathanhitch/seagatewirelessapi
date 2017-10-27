f3acf5153500b92b5927e1a1ef5dd36d2e1026b1
nathanhitch <pokemon25124@gmail.com>
2017-10-27 19:40:11 +1030

    first commit

---
+++ api.py
@@ -0,0 +1,44 @@
+#----imports start----#
+import urllib.request
+#----imports end----#
+ip = "172.25.0.1"
+
+def set_ip(x):
+    global ip
+    ip = x
+    
+def getAPIVersions():
+     url = "http://" + ip + "/getAPIVersions.psp"
+     req = urllib.request.urlopen(url)
+     data = req.read().decode('utf-8')
+     return data
+     
+def getDataRates():
+    url = "http://" + ip + "/getDataRates.psp"
+    req = urllib.request.urlopen(url)
+    data = req.read().decode('utf-8')
+    return data
+    
+def getHDDStat():
+    url = "http://" + ip + "/getHDDStat.psp"
+    req = urllib.request.urlopen(url)
+    data = req.read().decode('utf-8')
+    return data
+    
+def getscannednetworks():
+    url = "http://" + ip + "/get-scanned-networks.psp"
+    req = urllib.request.urlopen(url)
+    data = req.read().decode('utf-8')
+    return data
+    
+def getMaxClient():
+    url = "http://" + ip + "/getMaxClient.psp"
+    req = urllib.request.urlopen(url)
+    data = req.read().decode('utf-8')
+    return data
+
+def getLogs():
+    url = "http://" + ip + "/getLogs.psp"
+    req = urllib.request.urlopen(url)
+    data = req.read().decode('utf-8')
+    return data
