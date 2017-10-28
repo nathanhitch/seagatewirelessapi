85a7f45a826c42cda7846400f6f052db4e200c49
nathanhitch <pokemon25124@gmail.com>
2017-10-28 21:55:49 +1030

    converted json to Dictionaries

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 #----imports start----#
 import urllib.request
+import json
 #----imports end----#
 ip = "172.25.0.1"
 
@@ -11,46 +12,46 @@ def getAPIVersions():
      url = "http://" + ip + "/getAPIVersions.psp"
      req = urllib.request.urlopen(url)
      data = req.read().decode('utf-8')
-     return data
+     return json.loads(data)
      
 def getDataRates():
     url = "http://" + ip + "/getDataRates.psp"
     req = urllib.request.urlopen(url)
     data = req.read().decode('utf-8')
-    return data
+    return json.loads(data)
     
 def getHDDStat():
     url = "http://" + ip + "/getHDDStat.psp"
     req = urllib.request.urlopen(url)
     data = req.read().decode('utf-8')
-    return data
+    return json.loads(data)
     
 def getscannednetworks():
     url = "http://" + ip + "/get-scanned-networks.psp"
     req = urllib.request.urlopen(url)
     data = req.read().decode('utf-8')
-    return data
+    return json.loads(data)
     
 def getMaxClient():
     url = "http://" + ip + "/getMaxClient.psp"
     req = urllib.request.urlopen(url)
     data = req.read().decode('utf-8')
-    return data
+    return json.loads(data)
 
 def getLogs():
     url = "http://" + ip + "/getLogs.psp"
     req = urllib.request.urlopen(url)
     data = req.read().decode('utf-8')
-    return data
+    return json.loads(data)
 
 def getTime():
     url = "http://" + ip + "/getTime.psp"
     req = urllib.request.urlopen(url)
     data = req.read().decode('utf-8')
-    return data
+    return json.loads(data)
 
 def batteryStatus():
     url = "http://" + ip + "/batteryStatus.psp"
     req = urllib.request.urlopen(url)
     data = req.read().decode('utf-8')
-    return data
+    return json.loads(data)
